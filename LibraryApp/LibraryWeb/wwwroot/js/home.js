1ce036693834ad6749e27f0abbc48f6f437cde24
IliaKataev <163750267+IliaKataev@users.noreply.github.com>
2025-12-15 01:58:32 +0500

    автор и edit

--- LibraryApp/LibraryWeb/wwwroot/js/home.js
+++ LibraryApp/LibraryWeb/wwwroot/js/home.js
@@ -16,10 +16,17 @@
                     <p><strong>First Publish Date:</strong> ${book.firstPublishDate ?? ''}</p>
                     <p><strong>Description:</strong> ${book.description ?? ''}</p>
         `;
+        
 
         if (book.subjects.length) {
             html += `<p><strong>Subjects:</strong> ${book.subjects.map(s => s.subject).join(", ")}</p>`;
         }
+
+        html += `
+            <button class="btn btn-primary mt-3" id="show-authors-btn">
+                Посмотреть авторов
+            </button>
+        `;
 
         html += `</div>`; // закрываем блок текста
 
@@ -30,9 +37,15 @@
                 html += `<img src="/images/covers/${c.coverFile}.png" alt="Cover" style="max-width:100%; max-height:300px; object-fit:contain; border-radius:8px;" />`;
             });
         }
+        
+
         html += `</div></div>`; // закрываем контейнер
 
         detailsDiv.innerHTML = html;
+
+        document.getElementById("show-authors-btn")
+            ?.addEventListener("click", () => openAuthorsModal(book.authors));
+
     }
 
     // Двойной клик по строке
@@ -53,4 +66,35 @@
             row.style.display = title.includes(titleFilter) && authors.includes(authorFilter) ? "" : "none";
         });
     });
+
+    async function openAuthorsModal(authors) {
+        const select = document.getElementById("authors-select");
+        const details = document.getElementById("author-details");
+
+        select.innerHTML = `<option value="">Выберите автора</option>`;
+        details.innerHTML = `<p class="text-muted">Выберите автора, чтобы увидеть информацию</p>`;
+
+        authors.forEach(a => {
+            select.innerHTML += `<option value="${a.key}">${a.name}</option>`;
+        });
+
+        const modal = new bootstrap.Modal(document.getElementById("authorsModal"));
+        modal.show();
+    }
+    document.getElementById("authors-select").addEventListener("change", async (e) => {
+        const key = e.target.value;
+        if (!key) return;
+
+        const response = await fetch(`https://localhost:7055/api/authors/${key}`);
+        const author = await response.json();
+        console.log(author);
+
+        document.getElementById("author-details").innerHTML = `
+        <h5>${author.name}</h5>
+        <p><strong>Дата рождения:</strong> ${author.birthDate ?? '—'}</p>
+        <p><strong>Биография:</strong> ${author.bio ?? 'Нет информации'}</p>
+        <p><strong>Википедия:</strong> <a href="${author.wikipedia}">${author.wikipedia ?? 'Отсутствует ссылка'}</a></p>
+    `;
+    });
+
 });
